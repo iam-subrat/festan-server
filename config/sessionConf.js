6bf43aae5df2f159673aa84535f23537f6c62c11
Subrat Kumar Pattanaik <37294184+su-brat@users.noreply.github.com>
2024-03-29 12:58:30 +0530

    added cross-origin cookies support for prod (#6)

--- config/sessionConf.js
+++ config/sessionConf.js
@@ -1,18 +1,18 @@
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 
 const weekinmillis = (weeks = 1) => 1000 * 60 * 60 * 24 * 7 * weeks;
 
 const sessionConfig = {
-    name: 'user-session',
-    secret: process.env.SESSION_SECRET,
-    secure: process.env.NODE_ENV === 'PRODUCTION',
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.DB_PATH }),
-    cookie: {
-        expires: Date.now() + weekinmillis(),
-        maxAge: weekinmillis()
-    }
-}
+  name: "user-session",
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.DB_PATH }),
+  cookie: {
+    maxAge: weekinmillis(),
+    secure: process.env.NODE_ENV === "PRODUCTION",
+    sameSite: process.env.NODE_ENV === "PRODUCTION" ? "none" : false,
+  },
+};
 
 module.exports = sessionConfig;
