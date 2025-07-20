557e95df4b33c714d6ec892f77af1697612cf06e
Subrat Kumar Pattanaik <subratpatnaik.email@gmail.com>
2025-07-20 12:17:48 +0530

    release fixes (#21)
    * added cross-origin cookies support for prod (#6)
    
    * Revert "fix: pass samesite header with none value for all env (#15)" (#17)
    
    This reverts commit cc64fe7e52d3ef0bab4268d7c3fc0814677d517b.
    
    * fix: pass samesite header with none value for all env
    
    * fix: underline effect on login and register btns
    
    * fix: login and css assets
    
    ---------
    
    Co-authored-by: Subrat Kumar Pattanaik <37294184+su-brat@users.noreply.github.com>

--- config/sessionConf.js
+++ config/sessionConf.js
@@ -3,16 +3,17 @@ const MongoStore = require("connect-mongo");
 const weekinmillis = (weeks = 1) => 1000 * 60 * 60 * 24 * 7 * weeks;
 
 const sessionConfig = {
-  name: "user-session",
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.DB_PATH }),
-  cookie: {
-    maxAge: weekinmillis(),
-    secure: process.env.NODE_ENV === "PRODUCTION",
-    sameSite: "none",
-  },
+    name: "user-session",
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_PATH }),
+    cookie: {
+        maxAge: weekinmillis(),
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        httpOnly: true,
+    },
 };
 
 module.exports = sessionConfig;
