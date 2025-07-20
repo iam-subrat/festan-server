4af3a128c1c56fc3dae452545be1dbe9fbc02b34
Justin Truman <37294184+su-brat@users.noreply.github.com>
2025-07-20 13:44:20 +0530

    fix: cors issues (expt)

--- config/envConf.js
+++ config/envConf.js
@@ -1,6 +1,6 @@
 const path = require("path");
 function config() {
-    if (process.env.NODE_ENV.toUpperCase() !== "PRODUCTION")
+    if (process.env.NODE_ENV?.toUpperCase() !== "PRODUCTION")
         require("dotenv").config({ path: path.join(__dirname, "../.env") });
 }
 module.exports = config;
